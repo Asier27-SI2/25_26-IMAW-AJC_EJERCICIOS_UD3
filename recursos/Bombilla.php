6d2e154cd3cb94bf09787d7a125545ad05b06208
Andriana <andri.ilkiv1@gmail.com>
2025-10-23 09:53:38 +0200

    FEAT: commit inicial

---
+++ recursos/Bombilla.php
@@ -0,0 +1,26 @@
+<?php
+
+class bombilla {
+    private bool $encendida;
+    
+    public function __construct() {
+		$this->encendida=false;
+    }
+
+    public function getEncendida(): bool {
+        return $this->encendida;
+    }
+    public encender(): void {
+        $this->encendida=true;
+        echo "<p><em>la bombilla esta ahora encendida</p></em>"."this->encendida es".$this->encendida
+    }
+    public apagar(): void {
+        $this->encendida=false;
+        echo "<p><em>la bombilla esta ahora apagada</p></em>"."this->apagada es".$this->encendida
+    }
+
+    //public cambiar_estado($nuevo_estado): void(
+    //   $this->encendida=$nuevo_estado;
+    //)
+}
+?>
---
+++ recursos/trigonometria.php
@@ -0,0 +1,9 @@
+<?php
+
+    public function calculahipotenusa (float $catetoA, float $catetoB): float{
+        $hipotenusa = sqrt($catetoA**2 + $catetoB**2)
+        return round($hipotenusa);
+    }
+
+    
+?>
